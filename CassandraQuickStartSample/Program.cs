b65560af3b7a7572c8ae2a8560a36602583bec19
Michał Wierzbiński <michal.wierzbinski95@gmail.com>
2019-01-23 16:40:49 +0100

    Add project files.

---
+++ CassandraQuickStartSample/Program.cs
@@ -0,0 +1,85 @@
+﻿using Cassandra;
+using Cassandra.Mapping;
+using System;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CassandraQuickStartSample
+{
+    public class Program
+    {
+        // Cassandra Cluster Configs
+        // Keyspace is your database name, table will be a container.
+        // That's how Cosmos DB maps your entities.
+        private const string KeyspaceName = "cosmoscassandra";
+
+        private const string UserName = "localhost";
+
+        private const string Password = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+
+        private const string CassandraContactPoint = "localhost";  // DnsName
+
+        private static readonly int CassandraPort = 10350;
+
+        public static void Main(string[] args)
+        {
+            // Connect to cassandra cluster  (Cassandra API on Azure Cosmos DB supports only TLSv1.2)
+            var options = new Cassandra.SSLOptions(SslProtocols.Tls12, true, ValidateServerCertificate);
+            options.SetHostNameResolver((ipAddress) => CassandraContactPoint);
+            Cluster cluster = Cluster.Builder().WithCredentials(UserName, Password).WithPort(CassandraPort).AddContactPoint(CassandraContactPoint).WithSSL(options).Build();
+            ISession session = cluster.Connect();
+
+            // Creating KeySpace and table
+            session.Execute($"DROP KEYSPACE IF EXISTS {KeyspaceName}");
+            session.Execute($"CREATE KEYSPACE {KeyspaceName} WITH REPLICATION = " + "{ 'class' : 'NetworkTopologyStrategy', 'datacenter1' : 1 };");
+            Console.WriteLine($"created keyspace {KeyspaceName}");
+            session.Execute($"CREATE TABLE IF NOT EXISTS {KeyspaceName}.user (user_id int PRIMARY KEY, user_name text, user_bcity text)");
+            Console.WriteLine(String.Format("created table user"));
+
+            session = cluster.Connect($"{KeyspaceName}");
+            IMapper mapper = new Mapper(session);
+
+            // Inserting Data into user table
+            mapper.Insert<User>(new User(1, "LyubovK", "Dubai"));
+            mapper.Insert<User>(new User(2, "JiriK", "Toronto"));
+            mapper.Insert<User>(new User(3, "IvanH", "Mumbai"));
+            mapper.Insert<User>(new User(4, "LiliyaB", "Seattle"));
+            mapper.Insert<User>(new User(5, "JindrichH", "Buenos Aires"));
+            Console.WriteLine("Inserted data into user table");
+
+            Console.WriteLine("Select ALL");
+            Console.WriteLine("-------------------------------");
+            foreach (User user in mapper.Fetch<User>("Select * from user"))
+            {
+                Console.WriteLine(user);
+            }
+
+            Console.WriteLine("Getting by id 3");
+            Console.WriteLine("-------------------------------");
+            User userId3 = mapper.FirstOrDefault<User>("Select * from user where user_id = ?", 3);
+            Console.WriteLine(userId3);
+
+            // Clean up of Table and KeySpace
+            session.Execute("DROP table user");
+            session.Execute($"DROP KEYSPACE {KeyspaceName}");
+
+            // Wait for enter key before exiting
+            Console.ReadLine();
+        }
+
+        public static bool ValidateServerCertificate(
+            object sender,
+            X509Certificate certificate,
+            X509Chain chain,
+            SslPolicyErrors sslPolicyErrors)
+        {
+            if (sslPolicyErrors == SslPolicyErrors.None)
+                return true;
+
+            Console.WriteLine("Certificate error: {0}", sslPolicyErrors);
+            // Do not allow this client to communicate with unauthenticated servers.
+            return false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+# Developing a .net app with Cassandra API using Azure Cosmos DB Emulator
+Azure Cosmos DB is Microsoft's globally distributed multi-model database service. You can quickly create and query document, table, key-value, and graph databases, all of which benefit from the global distribution and horizontal scale capabilities at the core of Azure Cosmos DB. 
+
+Cosmos Emulator now supports Cassandra API (src: http://blog.mashfords.com/2018/12/19/azure-cosmos-db-emulator-support-for-cassandra-api/), however no one bothered to mentioned how to run it and how to connect to it. The Cosmos DB Emulator documentation page also seems to be out of date, so I decided to figure it out on my own.
+
+This quick start demonstrates how to connect to Cosmos DB Emulator's Cassandra Endpoint. The code is based on https://github.com/Azure-Samples/azure-cosmos-db-cassandra-dotnet-getting-started. You'll build a user profile console app, output as shown in the following image, with sample data.
+
+## Running this sample
+
+1. Clone this repository/
+
+2. Open the CassandraQuickStartSample.sln solution and install the Cassandra .NET driver. Use the .NET Driver's NuGet package. From the Package Manager Console window in Visual Studio:
+
+```bash
+PM> Install-Package CassandraCSharpDriver
+```
+3. Go to your Cosmos DB Emulator install location and open PowerShell window in that location. Default install path is `C:\Program Files\Azure Cosmos DB Emulator`
+4. Cassandra Endpoint is not enabled by default. To enable it run Cosmos DB Emulator from CMD/PowerShell using following command: 
+```powershell
+.\CosmosDB.Emulator.exe /EnableCassandra
+```
+ 5. Cassandra Endpoint will be opened on port `10350` by default. If you want to change that port run Emulator wiht follwing command:
+ 
+ ```powershell
+.\CosmosDB.Emulator.exe /EnableCassandra /CassandraPort=<port_number>
+```
+6. Endpoints in the sample are preconfigured to run with the Emulator. You don't have to change anything.
+7. Compile and Run the project.
+
+8. Output Image: 
+
+![User Data](/img.PNG?raw=true "user data")
+
+## About the code
+The code included in this sample is intended to get you quickly started with a C# application using Cassandra C# driver that connects to Azure Cosmos DB with the Cassandra API.
+
+## More information
+
+- [Azure Cosmos DB](https://docs.microsoft.com/azure/cosmos-db/introduction)
+- [Cassandra DB](http://cassandra.apache.org/)
